908c5c1e65a480f02aaad3fd7c1fb252a7ed4cd3
Mentos4 <zandrixjanpongos0104@gmail.com>
2025-10-25 12:35:01 +0800

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,20 @@
+local gamelist_content = game:HttpGet("https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/gamelist.lua")
+if not gamelist_content then return end
+
+local success, Games = pcall(function()
+    return loadstring(gamelist_content)()
+end)
+
+if not success or type(Games) ~= "table" then return end
+
+local gameId = tostring(game.GameId)
+local placeId = tostring(game.PlaceId)
+
+local scriptURL = Games[gameId] or Games[placeId] -- check GameId first, then fallback to PlaceId
+
+if scriptURL then
+    loadstring(game:HttpGet(scriptURL))()
+else
+    warn("No supported script found for this game. GameId: " .. gameId .. ", PlaceId: " .. placeId)
+end
+
