969a68aab1f69b967a26f1fe5b6e503ac7c371a7
Mentos4 <zandrixjanpongos0104@gmail.com>
2025-10-25 12:41:53 +0800

    Add files via upload

---
+++ premium/gamelist.lua
@@ -0,0 +1,38 @@
+local Games = {
+    -- Anime Eternal
+    ["7882829745"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/premium/Script/AnimeEternalFinal.lua",
+    -- Anime Ranger X
+    ["6884266247"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/premium/Script/AnimeRangerXFinal.lua",
+    -- Swordburst 3 floors
+    ["4093155512"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/Script/Script_Swordburst3.lua",
+    -- Dungeon Heroes
+    ["7546582051"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/Script/Script_DungeonHeroes.lua",
+    -- Hypershot
+    ["5995470825"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/Script/Script_Hypershot.lua",
+    -- Build an Island
+    ["7541395924"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/Script/Script_BuildanIsland.lua",
+    -- Blue Heater 2
+    ["5803093656"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/Script/Script_BlueHeater2.lua",
+    -- Arise Crossover
+    ["7074860883"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/Script/Script_AriseCrossover.lua",
+    -- Restaurant Tycoon 3
+    ["7094518649"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/Script/Script_RestaurantTycoon3.lua",
+    -- Build a Zoo
+    ["8066283370"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/premium/Script/BuildaZooFinal.lua",
+    -- Fish It
+    ["6701277882"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/Script/Script_FishIt.lua",
+    -- Plant vs Brainrot
+    ["8316902627"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/Script/Script_PlantvsBrainrot.lua",
+}
+
+
+return Games
+
+
+
+
+
+
+
+
+
